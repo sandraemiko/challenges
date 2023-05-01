499209ea49a415c87816c6adceb4d5614353adc2
sandraemiko <sandraemiko@prof.educacao.sp.gov.br>
2023-05-01 20:43:01 -0300

    add angle

--- angle.c
+++ angle.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
 #include <math.h>
-
-/*
-Tendo o solo como plano XY e o eixo Z em direção ao centro da terra ... Crie 3 funções :
-
-float ang@(float acx, float acy, float acz);
-
-A função recebe os valores de aceleração em cada um dos eixos x', y', z' do acelerômetro 
-
-E retorna o ângulo em relação aos eixos da terra e do acelerômetro
-*/
-
-#include <stdio.h>
-#include <math.h>
 #include <stdint.h>
 
 ///////////////////////////////////float f_abs(float num)//////////////////////////////////////////
+/*The f_abs function is a mathematical function that returns the absolute value of a floating-point number.
+In other words, if the number is positive, it returns the number itself, otherwise, it returns the negative value of the number*/
+
 float f_abs(float num)
  {
     if (num < 0)
@@ -25,17 +15,21 @@ float f_abs(float num)
 }
 
 /////////////////////////////////float f_same_sign(float x, float y)/////////////////////////////////////
+/*The 'f_same_sign' function is a mathematical function that takes two floating-point values as input and 
+returns the absolute value of the first value if both values have the same sign (positive or negative), and 
+the first value itself if they have different signs*/
 
 float f_same_sign(float x, float y) 
 {
    if ((x < 0 && y > 0) || (x > 0 && y < 0)) 
         return (-x);
     else 
-        return (x);
-    
+        return (x); 
 }
 
 ///////////////////////////////float f_atan_taylor(float x)////////////////////////////////////////
+/*The function 'f_atan_taylor' is an implementation of the Taylor series for the arctangent function
+(atan). The Taylor series is an infinite series that can be used to approximate a function around a specific point.*/
 
 float f_atan_taylor(float x)
 {
@@ -48,7 +42,7 @@ float f_atan_taylor(float x)
     result = x;
     term = x;
     i = 3;
-    while(f_abs(term) > 1e-7f)
+    while(f_abs(term) > 1e-10f)
     {
         term = term * (-squared) / ((float)i);
         result += term;
@@ -59,6 +53,9 @@ float f_atan_taylor(float x)
 }
 
 //////////////////////////////////float f_power(float b, int exp)////////////////////////////////////////
+/*The "f_power" function is a mathematical function that calculates the power of a real number "b" raised
+ to an integer exponent "exp".*/
+
 
 float f_power(float b, int exp) 
 {
@@ -76,6 +73,8 @@ float f_power(float b, int exp)
 }
 
 ///////////////////////////////float atan_chebyshev(float x)///////////////////////////////////////
+/*The function "f_atan_chebyshev" is an implementation of the Chebyshev approximation series method
+to calculate the arctangent function (atan) of a real number "x"*/
 
 float f_atan_chebyshev(float x)
 {
@@ -96,7 +95,7 @@ float f_atan_chebyshev(float x)
         squared = x * x;
         t = 0;
         u = 0;
-        i = 14;        
+        i = 1000;        
         while(i >= 0)
         {
             t = 1 / (2 * i + 1);
@@ -109,6 +108,15 @@ float f_atan_chebyshev(float x)
 }
 
 /////////////////////////////////float atanf(float x)//////////////////////////////////////////////////
+/*The function "atanf" is a mathematical function that calculates the arctangent (atan) of a real number
+ "x". It uses different approximation methods depending on the value of "x". If the absolute value of "x" 
+ is less than 0.1, the "f_atan_taylor" function is used. Otherwise, if "x" is between 0.1 and 1.0, the 
+ "f_atan_chebyshev" function is used. If "x" is between -1.0 and -0.1, the "f_atan_chebyshev" function 
+ is used with the argument -x and the result is negative. If "x" is greater than 1.0, the "f_atan_chebyshev" 
+ function is used with the argument 1/x and the result is adjusted to return the arctangent of the original 
+ value. If "x" is less than -1.0, the "f_atan_chebyshev" function is used with the argument 1/(-x) and the 
+ result is adjusted to return the negative of the arctangent of the original value. If "x" is equal to 0,
+ the function returns 0.*/
 
 float atanf(float x)
 {
@@ -135,13 +143,16 @@ float atanf(float x)
     return (result);
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*The "f_isnan" function is a function that checks whether a floating-point number "x" is a NaN (Not a Number) 
+value. It uses a property of the NaN value, which is that it is the only value that is different from itself.*/
 
 int f_isnan(double x)
 {
     return (x != x);
 }
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////int f_isinf(double x)///////////////////////////////////////////
+/*The function "f_isinf" is a function that checks if a floating-point number "x" is an infinite value.*/
 
 int f_isinf(double x)
 {
@@ -149,6 +160,12 @@ int f_isinf(double x)
 }
 
 //////////////////////////////////////float f_loor(float x)///////////////////////////////////////////////
+/*The function 'f_loor' is a mathematical function that returns the largest integer less than or equal to 
+a floating-point number 'x'. It does this by converting 'x' to an integer 'i' and then checking if 'x' is
+negative and has a non-zero fractional part. In this case, 'i' is decremented by 1 to obtain the largest
+integer less than or equal to 'x'. The function returns the converted value of 'i' back to a floating-point
+number.*/
+
 float f_loor(float x)
 {
     int i;
@@ -160,6 +177,7 @@ float f_loor(float x)
 }
 
 //////////////////////////////////int f_signbit(float x)/////////////////////////////////////////////
+/*The function "f_signbit" is a function that determines the sign of a floating-point number "x".*/
 
 int f_signbit(float x)
  {
@@ -173,6 +191,8 @@ int f_signbit(float x)
 
 
 /////////////////////////////float f_mod(float x, float y)////////////////////////////////////////////
+/*The function "f_mod" is a mathematical function that returns the remainder of dividing a floating-point
+number "x" by another floating-point number "y".*/
 
 float f_mod(float x, float y)
 {
@@ -193,6 +213,12 @@ float f_mod(float x, float y)
 }
 
 /////////////////////////////////////float f_atan(float y, float x)///////////////////////////////////////////
+/*The function "f_atan" is a mathematical function that calculates the arctangent of two floating-point numbers
+"y" and "x" and returns the result in radians. The function begins by checking if the arguments are NaN 
+(not a number) or infinite, returning NaN if this is the case. It then determines the quadrant of the Cartesian 
+coordinate (x, y) by calculating the angle with the "atanf" function and adjusting the result based on the quadrant
+to obtain the correct angle in the Cartesian plane. Finally, the function ensures that the resulting angle is within
+the interval [-π, π] and returns it.*/
 
 float f_atan(float y, float x)     
 {
@@ -226,6 +252,7 @@ float f_atan(float y, float x)
 }
 
 ////////////////////////////float f_sqrt_newton(float n, float x, float tol)//////////////////////////////////////////
+/*The function "f_sqrt_newton" is a function that calculates the square root of a floating-point number "n" using the Newton-Raphson method.*/
 
 float f_sqrt_newton(float n, float x, float tol) 
 {
@@ -246,6 +273,8 @@ float f_sqrt_newton(float n, float x, float tol)
 
 
 ///////////////////////////////////////////float f_acosf(float x) ///////////////////////////////////////////
+/*The "f_acosf" function is a mathematical function that calculates the arccosine of a floating-point number
+"x" and returns the result in radians.*/
 
 float f_acosf(float x) 
 {
@@ -272,6 +301,11 @@ float f_acosf(float x)
 }
 
 ////////////////////////////////////float ang_x(float acx, float acy, float acz)////////////////////////////////////////
+/*The "ang_x" function is a function that calculates the angle of a 3D vector represented by its x, y, and z coordinates.
+First, the function calculates the magnitude of the vector using the Newton-Raphson method for square root. Then, it 
+calculates the cosine of the angle by dividing the x coordinate by the value of the vector magnitude. The sine is then
+calculated from the cosine, again using the Newton-Raphson method. Finally, the function returns the arctangent of the
+sine and cosine to obtain the angle in radians.*/
 
 float ang_x(float acx, float acy, float acz)
 {
@@ -312,6 +346,10 @@ float ang_y(float acx, float acy, float acz)
 }
 
 /////////////////////////////////float ang_z(float acx, float acy, float acz)////////////////////////////////////////////
+/*The 'ang_z' function is a function that calculates the angle between a 3D vector and the z-axis. First, the function 
+calculates the magnitude of the vector using the Newton-Raphson method for square roots. Then, it calculates the cosine
+of the angle by dividing the z-coordinate by the value of the vector magnitude. Finally, the function returns the arccosine
+to obtain the angle in radians*/
 
 float ang_z(float acx, float acy, float acz) 
 {
@@ -326,8 +364,6 @@ float ang_z(float acx, float acy, float acz)
     cos = acz/G;
     return(f_acosf(cos));
 }
-
-
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 int main() {
--- teste.c
+++ teste.c
@@ -3,6 +3,9 @@
 #include <stdint.h>
 
 ///////////////////////////////////float f_abs(float num)//////////////////////////////////////////
+/*The f_abs function is a mathematical function that returns the absolute value of a floating-point number.
+In other words, if the number is positive, it returns the number itself, otherwise, it returns the negative value of the number*/
+
 float f_abs(float num)
  {
     if (num < 0)
@@ -12,6 +15,9 @@ float f_abs(float num)
 }
 
 /////////////////////////////////float f_same_sign(float x, float y)/////////////////////////////////////
+/*The 'f_same_sign' function is a mathematical function that takes two floating-point values as input and 
+returns the absolute value of the first value if both values have the same sign (positive or negative), and 
+the first value itself if they have different signs*/
 
 float f_same_sign(float x, float y) 
 {
@@ -22,6 +28,8 @@ float f_same_sign(float x, float y)
 }
 
 ///////////////////////////////float f_atan_taylor(float x)////////////////////////////////////////
+/*The function 'f_atan_taylor' is an implementation of the Taylor series for the arctangent function
+(atan). The Taylor series is an infinite series that can be used to approximate a function around a specific point.*/
 
 float f_atan_taylor(float x)
 {
@@ -45,6 +53,9 @@ float f_atan_taylor(float x)
 }
 
 //////////////////////////////////float f_power(float b, int exp)////////////////////////////////////////
+/*The "f_power" function is a mathematical function that calculates the power of a real number "b" raised
+ to an integer exponent "exp".*/
+
 
 float f_power(float b, int exp) 
 {
@@ -62,6 +73,8 @@ float f_power(float b, int exp)
 }
 
 ///////////////////////////////float atan_chebyshev(float x)///////////////////////////////////////
+/*The function "f_atan_chebyshev" is an implementation of the Chebyshev approximation series method
+to calculate the arctangent function (atan) of a real number "x"*/
 
 float f_atan_chebyshev(float x)
 {
@@ -95,6 +108,15 @@ float f_atan_chebyshev(float x)
 }
 
 /////////////////////////////////float atanf(float x)//////////////////////////////////////////////////
+/*The function "atanf" is a mathematical function that calculates the arctangent (atan) of a real number
+ "x". It uses different approximation methods depending on the value of "x". If the absolute value of "x" 
+ is less than 0.1, the "f_atan_taylor" function is used. Otherwise, if "x" is between 0.1 and 1.0, the 
+ "f_atan_chebyshev" function is used. If "x" is between -1.0 and -0.1, the "f_atan_chebyshev" function 
+ is used with the argument -x and the result is negative. If "x" is greater than 1.0, the "f_atan_chebyshev" 
+ function is used with the argument 1/x and the result is adjusted to return the arctangent of the original 
+ value. If "x" is less than -1.0, the "f_atan_chebyshev" function is used with the argument 1/(-x) and the 
+ result is adjusted to return the negative of the arctangent of the original value. If "x" is equal to 0,
+ the function returns 0.*/
 
 float atanf(float x)
 {
@@ -121,13 +143,16 @@ float atanf(float x)
     return (result);
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*The "f_isnan" function is a function that checks whether a floating-point number "x" is a NaN (Not a Number) 
+value. It uses a property of the NaN value, which is that it is the only value that is different from itself.*/
 
 int f_isnan(double x)
 {
     return (x != x);
 }
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////int f_isinf(double x)///////////////////////////////////////////
+/*The function "f_isinf" is a function that checks if a floating-point number "x" is an infinite value.*/
 
 int f_isinf(double x)
 {
@@ -135,6 +160,12 @@ int f_isinf(double x)
 }
 
 //////////////////////////////////////float f_loor(float x)///////////////////////////////////////////////
+/*The function 'f_loor' is a mathematical function that returns the largest integer less than or equal to 
+a floating-point number 'x'. It does this by converting 'x' to an integer 'i' and then checking if 'x' is
+negative and has a non-zero fractional part. In this case, 'i' is decremented by 1 to obtain the largest
+integer less than or equal to 'x'. The function returns the converted value of 'i' back to a floating-point
+number.*/
+
 float f_loor(float x)
 {
     int i;
@@ -146,6 +177,7 @@ float f_loor(float x)
 }
 
 //////////////////////////////////int f_signbit(float x)/////////////////////////////////////////////
+/*The function "f_signbit" is a function that determines the sign of a floating-point number "x".*/
 
 int f_signbit(float x)
  {
@@ -159,6 +191,8 @@ int f_signbit(float x)
 
 
 /////////////////////////////float f_mod(float x, float y)////////////////////////////////////////////
+/*The function "f_mod" is a mathematical function that returns the remainder of dividing a floating-point
+number "x" by another floating-point number "y".*/
 
 float f_mod(float x, float y)
 {
@@ -179,6 +213,12 @@ float f_mod(float x, float y)
 }
 
 /////////////////////////////////////float f_atan(float y, float x)///////////////////////////////////////////
+/*The function "f_atan" is a mathematical function that calculates the arctangent of two floating-point numbers
+"y" and "x" and returns the result in radians. The function begins by checking if the arguments are NaN 
+(not a number) or infinite, returning NaN if this is the case. It then determines the quadrant of the Cartesian 
+coordinate (x, y) by calculating the angle with the "atanf" function and adjusting the result based on the quadrant
+to obtain the correct angle in the Cartesian plane. Finally, the function ensures that the resulting angle is within
+the interval [-π, π] and returns it.*/
 
 float f_atan(float y, float x)     
 {
@@ -212,6 +252,7 @@ float f_atan(float y, float x)
 }
 
 ////////////////////////////float f_sqrt_newton(float n, float x, float tol)//////////////////////////////////////////
+/*The function "f_sqrt_newton" is a function that calculates the square root of a floating-point number "n" using the Newton-Raphson method.*/
 
 float f_sqrt_newton(float n, float x, float tol) 
 {
@@ -232,6 +273,8 @@ float f_sqrt_newton(float n, float x, float tol)
 
 
 ///////////////////////////////////////////float f_acosf(float x) ///////////////////////////////////////////
+/*The "f_acosf" function is a mathematical function that calculates the arccosine of a floating-point number
+"x" and returns the result in radians.*/
 
 float f_acosf(float x) 
 {
@@ -258,6 +301,11 @@ float f_acosf(float x)
 }
 
 ////////////////////////////////////float ang_x(float acx, float acy, float acz)////////////////////////////////////////
+/*The "ang_x" function is a function that calculates the angle of a 3D vector represented by its x, y, and z coordinates.
+First, the function calculates the magnitude of the vector using the Newton-Raphson method for square root. Then, it 
+calculates the cosine of the angle by dividing the x coordinate by the value of the vector magnitude. The sine is then
+calculated from the cosine, again using the Newton-Raphson method. Finally, the function returns the arctangent of the
+sine and cosine to obtain the angle in radians.*/
 
 float ang_x(float acx, float acy, float acz)
 {
@@ -298,6 +346,10 @@ float ang_y(float acx, float acy, float acz)
 }
 
 /////////////////////////////////float ang_z(float acx, float acy, float acz)////////////////////////////////////////////
+/*The 'ang_z' function is a function that calculates the angle between a 3D vector and the z-axis. First, the function 
+calculates the magnitude of the vector using the Newton-Raphson method for square roots. Then, it calculates the cosine
+of the angle by dividing the z-coordinate by the value of the vector magnitude. Finally, the function returns the arccosine
+to obtain the angle in radians*/
 
 float ang_z(float acx, float acy, float acz) 
 {
@@ -313,18 +365,16 @@ float ang_z(float acx, float acy, float acz)
     return(f_acosf(cos));
 }
 
-
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 int main() {
-    printf("%f\n", atanf(2/1)); // output: 0.785398
-    printf("%fresultado\n", f_atan(2,1)); // output: 0.785398
-    printf("%fui\n", atan2(2,1)); // output: 0.785398
+    printf("%f\n", atanf(2/1)); 
+    printf("%fresultado\n", f_atan(2,1)); 
+    printf("%fui\n", atan2(2,1));
 
-    printf("%f\n", atanf(0.5f)); // output: 0.463648
-    printf("%f\n", atanf(2)); // output: 1.107149
-    printf("%f\n", atanf(-0.5f)); // output: -0.463648
-    printf("%f\n", atanf(-2500)); // output: -1.107149
+    printf("%f\n", atanf(0.5f)); 
+    printf("%f\n", atanf(2)); 
+    printf("%f\n", atanf(-0.5f)); 
+    printf("%f\n", atanf(-2500)); 
     printf("%f\n", atan(-2500));
 
     printf("%f acos\n", f_acosf(0.5));
